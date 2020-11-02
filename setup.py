2be731ad98be0ddbf1b4c82f36d0a1b7f9418bec
Peter Evans <peter.evans@cerberusnuclear.com>
2020-11-02 11:24:05 +0000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="eddy-mc-peter-evans", # Replace with your own username
+    version="0.1.0",
+    author="Cerberus Nuclear",
+    author_email="nuclear@cerberusnuclear.com",
+    description="Eddy, the MCNP and SCALE HTML output converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cerberus-Nuclear/Eddy-Source",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GPL Version 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
