f0966f996caf684be4989c8553277576f3d9f054
Peter Evans <peter.evans@cerberusnuclear.com>
2020-11-17 09:37:29 +0000

    setup.py version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="eddy_mc", 
-    version="0.2.0",
+    version="0.1.1",
     author="Cerberus Nuclear",
     author_email="nuclear@cerberusnuclear.com",
     description="Eddy, the MCNP and SCALE HTML output converter",
